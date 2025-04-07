4f734a02c67f49b2b473ddbc9acff628f1104058
joshbesse <joshua.besse3@gmail.com>
2025-04-07 12:09:22 -0400

    3 column page layout and added header and results table

--- app.py
+++ app.py
@@ -1,32 +1,73 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
 
 @st.cache_data
 def load_data_for_year(year):
-    return pd.read_pickle(f'./data/{year}_election.pkl')
+    df_year = pd.read_pickle(f'./data/{year}_election.pkl')
+    df_results = pd.read_pickle('./data/results.pkl')
+    df_results = df_results[df_results['year'] == int(f'{year}')]
 
+    return df_year, df_results
+
+def render_outcome(df_result):
+    row = df_result.iloc[0]
+
+    html = f"""
+    <div style='padding: 1em; border-radius: 10px; font-family: sans-serif; margin-bottom: 0;'>
+        <div style='font-weight: bold; display: flex; justify-content: space-between; padding: 0.3em 0; border-bottom: 1px solid #ccc;'>
+            <div style='flex: 1;'>Candidate</div>
+            <div style='flex: 1;'>Electoral Vote</div>
+            <div style='flex: 1;'>Popular Vote</div>
+        </div>
+
+        <div style='display: flex; justify-content: space-between; padding: 0.4em 0; color: {row['win_party_color']}; font-weight: 600;'>
+            <div style='flex: 1;'>✅ {row['win_candidate']}</div>
+            <div style='flex: 1;'>{row['win_ele_votes']} ({row['win_ele_votes_pct']}%)</div>
+            <div style='flex: 1;'>{row['win_pop_votes']:,} ({row['win_pop_votes_pct']}%)</div>
+        </div>
+
+        <div style='display: flex; justify-content: space-between; padding: 0.1em 0; color: {row['lose_party_color']};'>
+            <div style='flex: 1;'>{row['lose_candidate']}</div>
+            <div style='flex: 1;'>{row['lose_ele_votes']} ({row['lose_ele_votes_pct']}%)</div>
+            <div style='flex: 1;'>{row['lose_pop_votes']:,} ({row['lose_pop_votes_pct']}%)</div>
+        </div>
+    </div>
+    """
+
+    components.html(html, height=110)
+
+# create page layout (3 columns)
+st.set_page_config(layout='wide')
+c1, c2, c3 = st.columns([1, 5, 2])
+
+# COLUMN 1 - election year selection
 # radio button for year selection
-selected_year = st.sidebar.radio('Select Year', range(1976, 2021, 4))
+selected_year = c1.radio('Select Year', range(1976, 2021, 4))
 
+# COLUMN 2 - election map
 # load data for chosen year
-df_year = load_data_for_year(selected_year)
+df_year, df_result = load_data_for_year(selected_year)
 
-# create election map for chosen year
-fig = go.Figure(data=go.Choropleth(
-    locations=df_year['state_code'],
-    z=df_year['winning_party'].map({'DEMOCRAT': 0, 'REPUBLICAN': 1}),
-    locationmode='USA-states',
-    colorscale=[[0, 'blue'], [1, 'red']],
-    showscale=False,
-    text=df_year['hover_text'],
-    hovertemplate="%{text}<extra></extra>"
-))
+with c2:
+    st.markdown(f"<h2 style='margin-bottom: -1em;'>1976 Election</h3>", unsafe_allow_html=True)
+    render_outcome(df_result)
 
-fig.update_layout(
-    geo_scope='usa',
-    margin=dict(l=0, r=0, t=0, b=0)
-)
+    # create election map for chosen year
+    fig = go.Figure(data=go.Choropleth(
+        locations=df_year['state_code'],
+        z=df_year['winning_party'].map({'DEMOCRAT': 0, 'REPUBLICAN': 1}),
+        locationmode='USA-states',
+        colorscale=[[0, 'blue'], [1, 'red']],
+        showscale=False,
+        text=df_year['hover_text'],
+        hovertemplate="%{text}<extra></extra>"
+    ))
 
-st.plotly_chart(fig, use_container_width=True)
+    fig.update_layout(
+        geo_scope='usa',
+        margin=dict(l=0, r=0, t=0, b=0)
+    )
 
+    st.plotly_chart(fig, use_container_width=True)
--- data.py
+++ data.py
@@ -1,26 +1,33 @@
 import pandas as pd
 
-def format_hover_text(row, candidates, state):
+def format_hover_text(row, candidates):
     c1, c2 = candidates[0], candidates[1]
 
-    def line(name, pct, votes):
-        return (
-            f"{name:<20}".replace(" ", "&nbsp;") +
-            f"{pct:>6.1f}%".rjust(10).replace(" ", "&nbsp;") +
-            f"{int(votes):,}".rjust(12).replace(" ", "&nbsp;")
-        )
+    def get_color(party):
+        if 'DEM' in party:
+            return 'blue'
+        else:
+            return 'red'
+
+    def line(name, pct, votes, party):
+        color = get_color(party)
+        name_html = f"{name:<20}".replace(" ", "&nbsp;")
+        pct_html = f"{pct:>6.1f}%".rjust(10).replace(" ", "&nbsp;")
+        vote_html = f"{int(votes):,}".rjust(12).replace(" ", "&nbsp;")
+
+        return f"<span style='color:{color}'>{name_html}{pct_html}{vote_html}</span>"
 
     header = (
-        "<b>" + state + "</b><br>" +
-        "<span style='font-family:Courier'>" +
+        "<b>" + row['state'] + "</b><br>" +
+        "<span style='font-family:Courier'><b>" +
         f"{'Candidate':<20}".replace(" ", "&nbsp;") +
         f"{'%':>6}".rjust(10).replace(" ", "&nbsp;") +
         f"{'Votes':>12}".rjust(12).replace(" ", "&nbsp;") +
-        "<br>"
+        "</b><br>"
     )
 
-    line1 = line(c1, row[f'pctvotes_{c1}'], row[f'candidatevotes_{c1}'])
-    line2 = line(c2, row[f'pctvotes_{c2}'], row[f'candidatevotes_{c2}'])
+    line1 = line(c1, row[f'pctvotes_{c1}'], row[f'candidatevotes_{c1}'], row[f'party_{c1}'])
+    line2 = line(c2, row[f'pctvotes_{c2}'], row[f'candidatevotes_{c2}'], row[f'party_{c2}'])
 
     return header + line1 + "<br>" + line2 + "</span>"
 
@@ -62,8 +69,15 @@ for year in df['year'].unique():
     pivot['winning_party'] = pivot.apply(lambda row: row[f'party_{candidates[0]}'] if row[f'pctvotes_{candidates[0]}'] > row[f'pctvotes_{candidates[1]}'] else row[f'party_{candidates[1]}'], axis=1)
 
     # create hover text for map (displayed when state is hovered on the map)
-    pivot['hover_text'] = pivot.apply(lambda row: format_hover_text(row, candidates, row['state']), axis=1)
+    pivot['hover_text'] = pivot.apply(lambda row: format_hover_text(row, candidates), axis=1)
 
     # save dataset for each year
     pivot.to_pickle(f'./data/{year}_election.pkl')
     print(f"Saved {year} election data.")
+
+# load election results with electoral vote and popular vote
+results = pd.read_excel('./data/Election_Results.xlsx')
+
+# save as pickle file for performance (faster to read)
+results.to_pickle('./data/results.pkl')
+print("Saved results data.")
